0ccc1866aa128054c32fcaed287d8dd04d08625f
thennek0 <kennethbecaro0@gmail.com>
2024-05-26 21:11:47 +0800

    feat: add env vars

--- src/main.py
+++ src/main.py
@@ -1,7 +1,19 @@
 import indeed.indeed_scraper as indeed
+import sys
+from dotenv import load_dotenv
 
 
 def main():
+
+    if len(sys.argv) < 2:
+        print('supply environment type of either "dev" or "prod"')
+        return
+
+    if sys.argv[1] == "prod":
+        load_dotenv(".env.production")
+    else:
+        load_dotenv(".env.development")
+
     indeed.scrape_indeed()
 
 
